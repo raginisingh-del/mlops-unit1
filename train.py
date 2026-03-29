a75334fab8b7afb7337fea5fc3dced208399f6cc
Namrata Singh <singhnamrata0122@gmail.com>
2026-03-29 19:01:50 +0530

    Added CI Pipeline

--- train.py
+++ train.py
@@ -1,35 +1,26 @@
-import mlflow
-import mlflow.sklearn
-
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import joblib
 
 # Load dataset
-iris = load_iris()
-X = iris.data
-y = iris.target
+data = load_breast_cancer()
+X = data.data
+y = data.target
 
-mlflow.set_experiment("Iris_Classification")
+# Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-with mlflow.start_run():
-    max_iter = 200
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
+# Model
+model = RandomForestClassifier(n_estimators=100, max_depth=5)
+model.fit(X_train, y_train)
 
-    model = LogisticRegression(max_iter=max_iter)
-    model.fit(X_train, y_train)
+# Predict
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
 
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
 
-    mlflow.log_param("max_iter", max_iter)
-
-    mlflow.log_metric("accuracy", accuracy)
-
-    mlflow.sklearn.log_model(model, "model")
-
-    print(f"Accuracy: {accuracy}")
+    
